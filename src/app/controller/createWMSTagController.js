b3d134fdf39341f9d31b26cdc13b3b1d760386cf
Theu <teuzin375@gmail.com>
2023-06-01 19:20:18 -0300

    fix: numbers without zero and building without side

--- src/app/controller/createWMSTagController.js
+++ src/app/controller/createWMSTagController.js
@@ -15,23 +15,28 @@ module.exports = {
       box_inicial,
       box_final } = request.body;
 
-      if (!filial || !edificio || !rua_inicial || !rua_final || !predio_inicial || !predio_final || !andar_inicial || !andar_final || !box_inicial || !box_final)
-          throw new AppError('Preencha todos os campos!');
+    if (!filial || !edificio || !rua_inicial || !rua_final || !predio_inicial || !predio_final || !andar_inicial || !andar_final || !box_inicial || !box_final)
+        throw new AppError('Preencha todos os campos!');
+
+    if (predio_inicial.toUpperCase().trim().slice(1) != predio_final.toUpperCase().trim().slice(1)) {
+      throw new AppError('Lados diferentes!');
+    }
 
     const data_res = gerarSequencia({
       filial: filial.toUpperCase().trim(),
       edificio: edificio.toUpperCase().trim(),
       ruas: {
-        min: Number(rua_inicial.split(0).join("")),
-        max: Number(rua_final.split(0).join(""))
+        min: Number(rua_inicial),
+        max: Number(rua_final)
       },
       predios: {
         min: predio_inicial.toUpperCase().trim(),
-        max: predio_final.toUpperCase().trim()
+        max: predio_final.toUpperCase().trim(),
+        direction: predio_inicial.toUpperCase().trim().slice(1)
       },
       andares: {
-        min: Number(andar_inicial.split(0).join("")),
-        max: Number(andar_final.split(0).join(""))
+        min: Number(andar_inicial),
+        max: Number(andar_final)
       },
       boxs: {
         min: box_inicial.toUpperCase().trim(),
--- src/helpers/generateSequencial.js
+++ src/helpers/generateSequencial.js
@@ -1,3 +1,7 @@
+function format(n) {
+  return `${n < 10 ? '0' + n : n}`
+}
+
 function gerarSequencia(dataWMS) {
   const arr = [];
 
@@ -5,7 +9,7 @@ function gerarSequencia(dataWMS) {
     for (let predio = dataWMS.predios.min.charCodeAt(0); predio <= dataWMS.predios.max.charCodeAt(0); predio++) {
       for (let andar = dataWMS.andares.min; andar <= dataWMS.andares.max; andar++) {
         for (let box = dataWMS.boxs.min.charCodeAt(0); box <= dataWMS.boxs.max.charCodeAt(0); box++) {
-          arr.push(`${dataWMS.filial} ${dataWMS.edificio} ${rua < 10 ? '0' + rua : rua} ${String.fromCharCode(predio)}E ${andar < 10 ? '0' + andar : andar} ${String.fromCharCode(box)}`);
+          arr.push(`${dataWMS.filial} ${dataWMS.edificio} ${format(rua)} ${String.fromCharCode(predio) + dataWMS.predios.direction} ${format(andar)} ${String.fromCharCode(box)}`);
         }
       }
     }
